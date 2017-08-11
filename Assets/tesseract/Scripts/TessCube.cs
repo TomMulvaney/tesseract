5ca8992dd567bc01b33fb7ccecbe09efffda0700
TomMulvaney <mulvaney.tom@gmail.com>
2017-08-11 18:54:25 +0100

    Created tesseract

---
+++ Assets/tesseract/Scripts/TessCube.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TessCube : MonoBehaviour {
+
+    public TessCube up;
+    public TessCube down;
+    public TessCube left;
+    public TessCube right;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
